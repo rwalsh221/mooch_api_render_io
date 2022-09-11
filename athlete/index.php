35bc0eb40d585143b1fc991c3af17d939067a61e
Richard Walsh <richwalsh2@googlemail.com>
2022-09-11 15:01:45 +0100

    initial commit

---
+++ athlete/index.php
@@ -0,0 +1,13 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+
+require dirname(__DIR__, 1) . '/includes/classes/curl/CurlSettings.php';
+
+$authToken = '3a38f64e0d147f1527fb805ea5739bd0cd2efd91';
+
+$getAthlete = new CurlSettings('/athlete', array(
+    'Content-Type: application/json', 'Authorization: Bearer ' . $authToken));
+
+$getAthlete->curlInit();
+
+?>
---
+++ test/index.php
@@ -0,0 +1,25 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+// https://stackoverflow.com/questions/16700960/how-to-use-curl-to-get-json-data-and-decode-the-data
+
+$endpoint = 'https://jsonplaceholder.typicode.com/comments';
+
+
+$params = array('postId' => '1');
+// $result = file_get_contents($url);
+
+$url = $endpoint . '?' . http_build_query($params);
+
+$curlInit = curl_init();
+
+curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
+
+curl_setopt($curlInit, CURLOPT_URL, $url);
+
+$result = curl_exec($curlInit);
+
+curl_close($curlInit);
+
+echo $result;
+
+?>
